6677659215b10d1a561acd86e1fbf9b59e3db014
saya <uknow.love1229@gmail.com>
2023-08-14 11:35:39 +0900

    add: react-uuid

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
+import uuid from 'react-uuid';
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -9,7 +10,7 @@ function App() {
   const onAddNote = () => {
     console.log('新しくノートが追加されました');
     const newNote = {
-      id: 1,
+      id: uuid(),
       title: '新しいノート',
       content: '新しいノートの内容',
       modDate: Date.now(),
