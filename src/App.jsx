1fb665431e42ccbc2403f4e2eb961fd52891bfdc
saya <uknow.love1229@gmail.com>
2023-08-14 14:15:34 +0900

    add: strage-save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 import uuid from 'react-uuid';
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  );
   const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    //ローカルストレージにノートを保存する
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   const onAddNote = () => {
     console.log('新しくノートが追加されました');
