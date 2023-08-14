cc94ae8a12a88b41c60b036f0697b7e86bc6ab3e
saya <uknow.love1229@gmail.com>
2023-08-14 14:17:29 +0900

    add: first-pick

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,10 @@ function App() {
     localStorage.setItem('notes', JSON.stringify(notes));
   }, [notes]);
 
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, []);
+
   const onAddNote = () => {
     console.log('新しくノートが追加されました');
     const newNote = {
